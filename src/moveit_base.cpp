c8219e9f76df819629bc4182efa6d4ca77d77b2d
Dave Coleman <davetcoleman@gmail.com>
2017-01-05 16:00:52 -0800

    Add desc

--- src/moveit_base.cpp
+++ src/moveit_base.cpp
@@ -1,7 +1,7 @@
 /*********************************************************************
  * Software License Agreement (BSD License)
  *
- *  Copyright (c) 2015, PickNik LLC
+ *  Copyright (c) 2017, PickNik LLC
  *  All rights reserved.
  *
  *  Redistribution and use in source and binary forms, with or without
@@ -33,7 +33,7 @@
  *********************************************************************/
 
 /* Author: Dave Coleman <dave@dav.ee>
-   Desc:   Base class for using MoveIt! in C++
+   Desc:   Base boilerplate class for easily using MoveIt! in C++ with a single robot arm
 */
 
 // C++
